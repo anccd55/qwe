bbeb9f0872dabd91978d315e948e861b95c5d711
null null <anccd55@gmail.com>
2020-05-27 13:33:54 +0000

    test3

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ client = MongoClient(url)
 db = client['heroku_c31w8929']
 collection = db['test']
 
-collection.insert_one({'a':'b'})
-
+# collection.insert_one({'a':'b'})
 rows = collection.find({})
+result = []
+# collection.delete_many({'a':'b'})
 for row in rows:
-  print(row)
+   if 'age' in row:
+       if row['age'] > 21:
+          result.append(row['age'])
+
+max_age = max(result)
+
+rows = collection.find()
+max_age_result=[]
+for row in rows:
+   if 'age' in row:
+       if row['age'] == max_age:
+          max_age_result.append(row)
+print (len(max_age_result))
